529bc316e346c36c9d18ac8187b4011592fdf1c0
kuronetk123 <phongltt0203@gmail.com>
2025-10-14 10:46:01 +0700

    Update Database Atlas

--- config/db.js
+++ config/db.js
@@ -2,12 +2,13 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/metameal', {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://phongltt0203:bonnehihi123@ecchan.za1shup.mongodb.net/metameal?retryWrites=true&w=majority&appName=ecchan', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
 
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    console.log(`📊 Database: ${conn.connection.name}`);
   } catch (error) {
     console.error(`❌ MongoDB Connection Error: ${error.message}`);
     process.exit(1);
