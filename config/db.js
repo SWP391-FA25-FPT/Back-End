f1ba74a6a44b82a24f8d776848095a492fe65c08
toan215 <toantruongcong2005bee@gmail.com>
2025-10-26 23:07:36 +0700

    [CToan] update API recipe

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/first_space_db', {
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://phongltt0203:bonnehihi123@ecchan.za1shup.mongodb.net/metameal?retryWrites=true&w=majority&appName=ecchan', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
